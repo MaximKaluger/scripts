b6a59d74e85b5f2441f56599ecdb68f0f1e1cfd6
MaximKaluger <145701252+MaximKaluger@users.noreply.github.com>
2023-09-21 19:42:44 +0300

    Add files via upload

---
+++ Helloworld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helloworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       //Обычный коментарий:)
+       print("Проверка консоли"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
